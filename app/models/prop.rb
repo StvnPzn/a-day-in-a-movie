35bbbc4d01094c5e4f7a90f46e7d7724abb66055
Lisa Chatellier <78473267+shinboram-90@users.noreply.github.com>
2021-05-20 15:47:12 +0200

    front continuation, footer et alerts devise

--- app/models/prop.rb
+++ app/models/prop.rb
@@ -3,17 +3,4 @@ class Prop < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
   has_one_attached :photo
-
-  # def photo
-  #   @props = Prop.all
-  #   @props.each do |prop| 
-  #     unless prop.photo.attached?
-  #     prop.photo.attach(
-  #         io: File.open(Rails.root.join('app', 'assets', 'images', 'no_image_available.jpg')),
-  
-  #         filename: 'no_image_available.jpg', content_type: 'image/jpg'
-  #       )
-  #     end
-  #   end
-  # end
 end
